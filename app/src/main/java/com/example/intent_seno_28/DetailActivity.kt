d948b451aec0379d84dc21cc1342033149c0f741
Senp06 <ekasasurasenopati@gmail.com>
2021-11-27 16:24:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intent_seno_28/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intent_seno_28
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedIntanceState: Bundle?){
+        super.onCreate(savedIntanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
